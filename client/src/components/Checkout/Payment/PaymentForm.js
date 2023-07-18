f9e814dad3dc53acb37f28345107383be289cc4d
tejas <tejasftw117@gmail.com>
2023-07-18 14:53:07 +0530

    changed return url after payment

--- client/src/components/Checkout/Payment/PaymentForm.js
+++ client/src/components/Checkout/Payment/PaymentForm.js
@@ -75,7 +75,7 @@ function PaymentForm() {
       elements,
       redirect: "if_required",
       confirmParams: {
-        return_url: `${process.env.APP_URL}/orders`,
+        return_url: `https://ecommerce-website-khaki.vercel.app/orders`,
         payment_method_data: {
           billing_details: shipping, //billing address is same as shipping address
         },
